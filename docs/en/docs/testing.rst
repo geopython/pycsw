88b2e66909a529f2609b73ca9714f6849ed2347d
tomkralidis <tomkralidis@f6d6ccb1-4906-4145-b9cb-3503ec0a5639>
2011-09-14 20:33:03 +0000

    add testing framework (#20) -- second pass

--- docs/en/docs/testing.rst
+++ docs/en/docs/testing.rst
@@ -15,6 +15,70 @@ Compliance benchmarking is done via the OGC `Compliance & Interoperability Testi
 Tester
 ------
 
-You can also use the pycsw tester via your web browser to perform sample requests against your pycsw install.  The tester is located in ``tester/index.html``.
+The pycsw tester framework (in ``tester``) is a collection of testsuites to perform automated regession testing of the codebase.
+
+Running
+^^^^^^^
+
+The tester framework can be run from ``tester``:
+
+.. code-block:: bash
+
+  $ cd /path/to/pycsw
+  $ cd tester
+  $ python ./run_tests.py
+  # lots of output
+
+The tester runs HTTP GET and POST requests.  The expected output for each test can be found in ``expected``.  Results are categorized as ``passed``, ``failed``, or ``initialized``.  A summary of results is output at the end of the run.
+
+Failed Tests
+^^^^^^^^^^^^
+
+If a given test has failed, the output is saved in ``results``.  The resulting failure can be analyzed by running ``diff expected/name_of_test.xml results/name_of_test.xml`` to find variances.
+
+Test Suites
+^^^^^^^^^^^
+
+The tester framework is run against a series of 'suites' (in ``tester/suites``), each of which specifies a given configuration to test various functionality of the codebase.  Each suite is structured as follows:
+
+* ``tester/suite/default.cfg``: the configuration for the suite
+* ``tester/suite/post``: directory of XML documents for HTTP POST requests
+* ``tester/suite/get/requests.txt``: directory and text file of KVP for HTTP GET requests
+
+When the tester is invoked, the following operations are run:
+
+* pycsw configuration is set to ``tester/suite/default.cfg``
+* HTTP POST requests are run against ``tester/suite/post/*.xml``
+* HTTP GET requests are run against each request in ``tester/suite/get/requests.txt``
+
+The CSV format of ``tester/suite/get/requests.txt`` is ``testname,request``, with one line for each test.  The ``testname`` value is a unique test name (this value sets the name of the output file in the test results).  The ``request`` value is the HTTP GET request.  The ``PYCSW_SERVER`` token is replaced at runtime with the URL to the pycsw install.
+
+Adding New Tests
+^^^^^^^^^^^^^^^^
+
+To add tests to an existing suite:
+
+* for HTTP POST tests, add XML documents to ``tester/suite/post``
+* for HTTP GET tests, add tests (one per line) to ``tester/suite/get/requests.txt``
+* run ``python ./run_tests.py <url>``
+
+To add a new test suite:
+
+* create a new directory under ``tester/suites`` (e.g. ``foo``)
+* create a new configuration in ``tester/suites/foo/default.cfg``.  Ensure that all file paths are relative to ``path/to/pycsw``
+* populate HTTP POST requests in ``tester/suites/foo/post``
+* populate HTTP GET requests in ``tester/suites/foo/get/requests.txt``
+* run ``python ./run_tests.py <url>``
+
+Web Testing
+^^^^^^^^^^^
+
+You can also use the pycsw tester via your web browser to perform sample requests against your pycsw install.  The tester is located in ``tester/``.  To generate the HTML page, run ``gen_html.py``:
+
+.. code-block:: bash
+
+  $ python ./gen_html.py > index.html
+
+Then navigate to ``http://host/path/to/pycsw/tester/index.html``.
 
 .. _`Compliance & Interoperability Testing & Evaluation Initiative`: http://cite.opengeospatial.org/
---
+++ tester/run_tests.py
@@ -0,0 +1,198 @@
+#!/usr/bin/python
+# =================================================================
+#
+# $Id$
+#
+# Authors: Tom Kralidis <tomkralidis@hotmail.com>
+#
+# Copyright (c) 2011 Tom Kralidis
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# =================================================================
+
+# simple testing framework inspired by MapServer msautotest
+
+import csv, sys, os, urllib2, httplib, glob, urlparse, filecmp, re
+
+def plural(num):
+    ''' Determine plurality given an integer '''
+    if num != 1:
+        return 's'
+    else:
+        return ''
+
+def http_req(method, url, request, config=None):
+    ''' Perform HTTP request '''
+    if method == 'POST':
+        '''Send an XML document as a HTTP POST request'''
+        u = urlparse.urlsplit(url)
+        h = httplib.HTTP(u.netloc)
+        h.putrequest('POST', u.path)
+        h.putheader('Content-type', 'text/xml')
+        h.putheader('Content-length', '%d' % len(request))
+        h.putheader('Accepts', 'text/xml')
+        h.putheader('Host', u.netloc)
+        if config is not None:
+            h.putheader('PYCSW_CONFIG', config)
+        h.putheader('User-Agent', 'pycsw unit tester')
+        h.endheaders()
+        h.send(request)
+        reply, msg, hdrs = h.getreply()
+        return h.getfile().read()
+    else:  # GET
+        req = urllib2.Request(url)
+        if config is not None:
+            req.add_header('PYCSW_CONFIG', config)
+        return urllib2.urlopen(req).read()
+
+def get_validity(expected, result, outfile):
+    ''' Decipher whether the output passes, fails, or initializes '''
+    if os.path.exists(expected) is False:  # create expected file
+        expectedfile = open(expected, 'w')
+        expectedfile.write(normalize(result))
+        expectedfile.close()
+        status = 0
+    else:  # compare result with expected
+        if os.path.exists('results') is False:
+            os.mkdir('results')
+        resultfile = open('results/%s' % outfile, 'w')
+        resultfile.write(normalize(result))
+        resultfile.close()
+        if filecmp.cmp(expected, 'results/%s' % outfile):  # pass
+            os.remove('results/%s' % outfile)
+            status = 1
+        else:  # fail
+            status = -1
+    return status
+
+def normalize(result):
+    ''' Replace time and version specific values with generic values '''
+    version = re.search('<!-- (.*) -->', result)
+    timestamp = re.search('timestamp="(.*)"', result)
+    timestamp2 = re.search('timeStamp="(.*)"', result)
+
+    if version:
+        result = result.replace(version.group(0),'<!-- PYCSW_VERSION -->')
+    if timestamp:
+        result = result.replace(timestamp.group(0),
+        'timestamp="PYCSW_TIMESTAMP"')
+    if timestamp2:
+        result = result.replace(timestamp2.group(0),
+        'timeStamp="PYCSW_TIMESTAMP"')
+
+    return result
+
+# main
+
+if len(sys.argv) < 2:
+    print 'Usage: %s <url> [log]' % sys.argv[0]
+    sys.exit(1)
+
+url = sys.argv[1]
+
+passed = 0
+failed = 0
+inited = 0
+
+logwriter = False
+
+print '\nRunning tests against %s' % url
+
+if len(sys.argv) == 3:  # write detailed output to CSV
+    logwriter = csv.writer(open(sys.argv[2], 'wb'))
+    logwriter.writerow(['url','configuration','testname','result'])
+
+for testsuite in glob.glob('suites/*'):
+    # get configuration
+    for cfg in glob.glob('%s/*.cfg' % testsuite):
+        print '\nTesting configuration %s' % cfg
+
+        for root, dirs, files in os.walk(testsuite):
+            if files:
+                for file in files:
+                    if os.path.splitext(file)[1] not in ['.xml','.txt']:
+                        break
+
+                    if file == 'requests.txt':  # GET requests
+                        gets = csv.reader(open('%s%s%s' % (root, os.sep, file)))
+                        for row in gets:
+                            testfile = '%s%s%s' % (root, os.sep, file)
+                            request = row[1].replace('PYCSW_SERVER', url)
+                            outfile = '%s%s' % (root.replace(os.sep, '_'),
+                            '_%s.xml' % row[0])
+                            expected = 'expected%s%s' % (os.sep, outfile)
+                            print '\n test %s:%s' % (testfile, row[0])
+
+                            result = http_req('GET', request, request,
+                            'tester%s%s' % (os.sep, cfg))
+
+                            status = get_validity(expected, result, outfile)
+
+                            if status == 1:
+                                print '  passed'
+                                passed += 1
+                            elif status == 0:
+                                print '  initialized'
+                                inited += 1
+                            else:
+                                print '  FAILED'
+                                failed += 1
+
+                            if logwriter:
+                                logwriter.writerow([url, cfg, testfile,status])
+
+                    else:  # POST requests
+                        testfile = '%s%s%s' % (root, os.sep, file)
+                        outfile = '%s%s' % (os.sep, testfile.replace(os.sep, '_'))
+                        expected = 'expected%s%s' % (os.sep, outfile)
+                        print '\n test %s' % testfile
+        
+                        # read test
+                        f = open(testfile)
+                        request = f.read()
+                        f.close()
+        
+                        # invoke request
+                        result = http_req('POST', url, request,
+                        'tester%s%s' % (os.sep, cfg))
+
+                        status = get_validity(expected, result, outfile)
+
+                        if status == 1:
+                            print '  passed'
+                            passed += 1
+                        elif status == 0:
+                            print '  initialized'
+                            inited += 1
+                        else:
+                            print '  FAILED'
+                            failed += 1
+        
+                        if logwriter:
+                            logwriter.writerow([url, cfg, testfile,status])
+    
+print '\nResults (%d/%d - %.2f%%)' % \
+(passed, passed+failed,float(passed)/float(passed+failed)*100)
+print '   %d test%s passed' % (passed, plural(passed))
+print '   %d test%s failed' % (failed, plural(failed))
+print '   %d test%s initialized' % (inited, plural(inited))
