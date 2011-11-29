8e1870fc7a718aea027efccb65e2390e63065091
tomkralidis <tomkralidis@f6d6ccb1-4906-4145-b9cb-3503ec0a5639>
2011-11-29 19:47:07 +0000

    repurpose axis order for geometry (#35)
    git-svn-id: https://pycsw.svn.sourceforge.net/svnroot/pycsw/trunk@369 f6d6ccb1-4906-4145-b9cb-3503ec0a5639

--- server/gml.py
+++ server/gml.py
@@ -32,6 +32,7 @@
 # =================================================================
 
 import util
+from owslib import crs
 
 TYPES = ['gml:Point', 'gml:LineString', 'gml:Polygon', 'gml:Envelope']
 
@@ -54,7 +55,7 @@ class Geometry(object):
         '|'.join(TYPES), namespaces={'gml':'http://www.opengis.net/gml'})[0]
 
         if operand.attrib.has_key('srsName'):
-            self.crs = operand.attrib['srsName']
+            self.crs = crs.Crs(operand.attrib['srsName'])
 
         self.type = util.xmltag_split(operand.tag)
 
@@ -66,7 +67,11 @@ class Geometry(object):
             self._get_polygon()
         elif util.xmltag_split(operand.tag) == 'Envelope':
             self._get_envelope()
-    
+
+        # reproject data if needed    
+        if self.crs.code != 4326:
+            self.wkt = self.transform(self.crs.code, DEFAULT_SRS.split(':')[-1])
+
     def _get_point(self):
         ''' Parse gml:Point '''
     
@@ -142,12 +147,40 @@ class Geometry(object):
 
         return ', '.join(poslist2)
     
-    def _geom_transform(src, dst, x, y):
+    def transform(self, src, dest):
         ''' transform coordinates from one CRS to another '''
     
         import pyproj
+        from shapely.geometry import Point, LineString, Polygon
+        from shapely.wkt import loads
     
-        p1 = pyproj.Proj(init='epsg:%s' % src)
-        p2 = pyproj.Proj(init='epsg:%s' % dst)
+        proj_src = pyproj.Proj(init='epsg:%s' % src)
+        proj_dst = pyproj.Proj(init='epsg:%s' % dest)
     
-        return pyproj.transform(p1, p2, x, y)
+        geom = loads(self.wkt)
+    
+        if geom.type == 'Point':
+            newgeom = Point(pyproj.transform(proj_src, proj_dst, geom.x, geom.y))
+            wkt2 = newgeom.wkt
+    
+        elif geom.type == 'LineString':
+            vertices = []
+            for vertice in list(geom.coords):
+                newgeom = pyproj.transform(proj_src, proj_dst, vertice[0], vertice[1])
+                vertices.append(newgeom)
+    
+            linestring = LineString(vertices)
+    
+            wkt2 = linestring.wkt
+    
+        elif geom.type == 'Polygon':
+            vertices = []
+            for vertice in list(geom.exterior.coords):
+                newgeom = pyproj.transform(proj_src, proj_dst, vertice[0], vertice[1])
+                vertices.append(newgeom)
+    
+            polygon = Polygon(vertices)
+    
+            wkt2 = polygon.wkt
+    
+        return wkt2
