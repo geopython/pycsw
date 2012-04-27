8f5c0ef125a3e91ab6c69caa9bf276398086905b
Tom Kralidis <tomkralidis@hotmail.com>
2012-04-27 19:45:36 +0000

    update doc publishing script

--- docs/publish.sh
+++ docs/publish.sh
@@ -1,1 +1,9 @@
-scp -rp build/html/en/* tomkralidis@projects.osgeo.osuosl.org:/osgeo/pycsw/pycsw-web
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 <OSGeo userid>"
+    exit 1
+fi
+
+scp -rp build/html/en/* $1@projects.osgeo.osuosl.org:/osgeo/pycsw/pycsw-web
