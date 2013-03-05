818b0911e81f23db2a650c28bba866170d754410
Simone Dalmasso <simone.dalmasso@gmail.com>
2013-03-05 13:34:15 +0100

    better fix for spatial

--- pycsw/plugins/repository/geonode/geonode_.py
+++ pycsw/plugins/repository/geonode/geonode_.py
@@ -127,7 +127,8 @@ class GeoNodeRepository(object):
                 desc = False
                 if sortby['order'] == 'DESC':
                     desc = True
-                return [str(total), sorted(query, key=lambda x: float(util.get_geometry_area(getattr(x, sortby['propertyname']))), reverse=desc).all()[startposition:maxrecords]]
+                query = query.all()
+                return [str(total), sorted(query, key=lambda x: float(util.get_geometry_area(getattr(x, sortby['propertyname']))), reverse=desc)[startposition:maxrecords]]
             else:
                 if sortby['order'] == 'DESC':
                     pname = '-%s' % sortby['propertyname']
--- pycsw/plugins/repository/odc/odc.py
+++ pycsw/plugins/repository/odc/odc.py
@@ -131,7 +131,8 @@ class OpenDataCatalogRepository(object):
                 desc = False
                 if sortby['order'] == 'DESC':
                     desc = True
-                return [str(total), sorted(query, key=lambda x: float(util.get_geometry_area(getattr(x, sortby['propertyname']))), reverse=desc).all()[startposition:maxrecords]]
+                query = query.all()
+                return [str(total), sorted(query, key=lambda x: float(util.get_geometry_area(getattr(x, sortby['propertyname']))), reverse=desc)[startposition:maxrecords]]
             if sortby['order'] == 'DESC':
                 pname = '-%s' % sortby['propertyname']
             else:
