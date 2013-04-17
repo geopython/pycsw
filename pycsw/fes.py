8f1df8b21318c41e7d3bbf10a1a1ffa2a1e2d75f
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2013-04-17 20:13:48 +0300

    Added option in configuration to enable spatial ranking (#141)

--- pycsw/fes.py
+++ pycsw/fes.py
@@ -318,20 +318,19 @@ def set_spatial_ranking(geometry):
     """Given that we have a spatial query in ogc:filter we check the type of geometry 
     and set the ranking variables"""
     
-    #TODO: Add a switch check to disable spatial ranking here
-    
-    if geometry.type in ['Polygon','Envelope']:
-	util.ranking_enabled = True
-	util.ranking_query_geometry = geometry.wkt
-    elif geometry.type == 'LineString':
-	from shapely.geometry.base import BaseGeometry
-	from shapely.geometry import box
-	from shapely.wkt import loads,dumps
-	ls = loads(geometry.wkt)
-	b = ls.bounds
-	tmp_box = box(b[0],b[1],b[2],b[3])
-	tmp_wkt = dumps(tmp_box)
-	if tmp_box.area > 0:
-	    util.ranking_enabled = True
-	    util.ranking_query_geometry = tmp_wkt
+    if util.ranking_enabled:
+	if geometry.type in ['Polygon','Envelope']:
+	    util.ranking_pass = True
+	    util.ranking_query_geometry = geometry.wkt
+	elif geometry.type == 'LineString':
+	    from shapely.geometry.base import BaseGeometry
+	    from shapely.geometry import box
+	    from shapely.wkt import loads,dumps
+	    ls = loads(geometry.wkt)
+	    b = ls.bounds
+	    tmp_box = box(b[0],b[1],b[2],b[3])
+	    tmp_wkt = dumps(tmp_box)
+	    if tmp_box.area > 0:
+		util.ranking_pass = True
+		util.ranking_query_geometry = tmp_wkt
 
