725b43c4a058410fd7cc1d338b2d1771df2035cb
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2013-04-18 03:02:12 +0300

    Bug fix: get WKT geometry column from repository.context (#141)

--- pycsw/repository.py
+++ pycsw/repository.py
@@ -192,8 +192,7 @@ class Repository(object):
         
         if util.ranking_pass:  #apply spatial ranking
 	    LOGGER.debug('spatial ranking detected')
-	    from pycsw import config
-	    query = query.order_by(func.get_spatial_overlay_rank(config.md_core_model['mappings']['pycsw:BoundingBox'], util.ranking_query_geometry).desc())
+	    query = query.order_by(func.get_spatial_overlay_rank(self.context.md_core_model['mappings']['pycsw:BoundingBox'], util.ranking_query_geometry).desc())
 	    #trying to make this wsgi safe
 	    util.ranking_pass = False
 	    util.ranking_query_geometry = ''
