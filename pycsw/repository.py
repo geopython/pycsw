a11cc38623bc0eea0ce1fe9120ed86c09769c931
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2014-02-10 22:54:04 +0200

    Adding meaningful message for native PostgreSQL not being picked up

--- pycsw/repository.py
+++ pycsw/repository.py
@@ -140,7 +140,8 @@ class Repository(object):
                 temp_dbtype = 'postgresql+postgis+native'
                 LOGGER.debug('PostgreSQL+PostGIS+Native detected')
             except:
-                pass
+                LOGGER.debug('PostgreSQL+PostGIS+Native not picked up', str(err))
+                #pass
 
             # check if a native PostgreSQL FTS GIN index exists
             result = self.session.execute("select relname from pg_class where relname='fts_gin_idx'").scalar()
