51fb08fc8fca531337d1bbd16c9d1c14c9b9d4d4
Tom Kralidis <tomkralidis@gmail.com>
2015-05-11 14:17:17 -0400

    first pass GetCapabilities handling

--- pycsw/core/config.py
+++ pycsw/core/config.py
@@ -53,15 +53,19 @@ class StaticContext(object):
         self.namespaces = {
             'atom': 'http://www.w3.org/2005/Atom',
             'csw': 'http://www.opengis.net/cat/csw/2.0.2',
+            'csw30': 'http://www.opengis.net/cat/csw/3.0',
             'dc': 'http://purl.org/dc/elements/1.1/',
             'dct': 'http://purl.org/dc/terms/',
             'dif': 'http://gcmd.gsfc.nasa.gov/Aboutus/xml/dif/',
+            'fes20': 'http://www.opengis.net/fes/2.0',
             'fgdc': 'http://www.opengis.net/cat/csw/csdgm',
             'gmd': 'http://www.isotc211.org/2005/gmd',
             'gml': 'http://www.opengis.net/gml',
             'ogc': 'http://www.opengis.net/ogc',
             'os': 'http://a9.com/-/spec/opensearch/1.1/',
             'ows': 'http://www.opengis.net/ows',
+            'ows11': 'http://www.opengis.net/ows/1.1',
+            'ows20': 'http://www.opengis.net/ows/2.0',
             'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
             'sitemap': 'http://www.sitemaps.org/schemas/sitemap/0.9',
             'soapenv': 'http://www.w3.org/2003/05/soap-envelope',
@@ -230,7 +234,7 @@ class StaticContext(object):
             },
             'parameters': {
                 'version': {
-                    'values': ['2.0.2']
+                    'values': ['2.0.2', '3.0.0']
                 },
                 'service': {
                     'values': ['CSW']
