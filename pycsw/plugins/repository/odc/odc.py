78e92d754db524422294e3a02a297babccd2a615
ocefpaf <ocefpaf@gmail.com>
2015-09-11 13:58:07 -0300

    Python 3 support

--- pycsw/plugins/repository/odc/odc.py
+++ pycsw/plugins/repository/odc/odc.py
@@ -28,6 +28,8 @@
 #
 # =================================================================
 
+from __future__ import (absolute_import, division, print_function)
+
 import os, sys
 
 from django.db import models
@@ -52,7 +54,7 @@ class OpenDataCatalogRepository(object):
         # ODC PostgreSQL installs are not PostGIS enabled
         if self.dbtype == 'postgresql_psycopg2':
             self.dbtype = 'postgresql'
-            
+
         if self.dbtype in ['sqlite', 'sqlite3']:  # load SQLite query bindings
             cursor = connection.cursor()
             connection.connection.create_function(
@@ -70,7 +72,7 @@ class OpenDataCatalogRepository(object):
                 self.queryables[qname] = {}
 
                 for qkey, qvalue in \
-                self.context.model['typenames'][tname]['queryables'][qname].iteritems():
+                self.context.model['typenames'][tname]['queryables'][qname].items():
                     self.queryables[qname][qkey] = qvalue
 
         # flatten all queryables
