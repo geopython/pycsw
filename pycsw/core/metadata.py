a2edc1fe5aa023df32cdf5a25e7157709e421087
James F. Dickens <jfdickens@gmail.com>
2016-04-18 10:43:01 -0500

    add WMTS harvesting

--- pycsw/core/metadata.py
+++ pycsw/core/metadata.py
@@ -4,6 +4,7 @@
 # Authors: Tom Kralidis <tomkralidis@gmail.com>
 #
 # Copyright (c) 2015 Tom Kralidis
+# Copyright (c) 2016 James F. Dickens
 #
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation
@@ -74,6 +75,10 @@ def parse_record(context, record, repos=None,
     elif mtype == 'http://www.opengis.net/wms':  # WMS
         LOGGER.debug('WMS detected, fetching via OWSLib')
         return _parse_wms(context, repos, record, identifier)
+
+    elif mtype == 'http://www.opengis.net/wmts/1.0':  # WMTS
+        LOGGER.debug('WMTS 1.0.0 detected, fetching via OWSLib')
+        return _parse_wmts(context, repos, record, identifier)
 
     elif mtype == 'http://www.opengis.net/wps/1.0.0':  # WPS
         LOGGER.debug('WPS detected, fetching via OWSLib')
@@ -413,6 +418,128 @@ def _parse_wms(context, repos, record, identifier):
         recobjs.append(recobj)
 
     return recobjs
+
+def _parse_wmts(context, repos, record, identifier):
+
+    from owslib.wmts import WebMapTileService
+
+    recobjs = []
+    serviceobj = repos.dataset()
+
+    md = WebMapTileService(record)
+    # generate record of service instance
+    _set(context, serviceobj, 'pycsw:Identifier', identifier)
+    _set(context, serviceobj, 'pycsw:Typename', 'csw:Record')
+    _set(context, serviceobj, 'pycsw:Schema', 'http://www.opengis.net/wmts/1.0')
+    _set(context, serviceobj, 'pycsw:MdSource', record)
+    _set(context, serviceobj, 'pycsw:InsertDate', util.get_today_and_now())
+    _set(context, serviceobj, 'pycsw:AnyText', util.get_anytext(md.getServiceXML()))
+    _set(context, serviceobj, 'pycsw:Type', 'service')
+    _set(context, serviceobj, 'pycsw:Title', md.identification.title)
+    _set(context, serviceobj, 'pycsw:Abstract', md.identification.abstract)
+    _set(context, serviceobj, 'pycsw:Keywords', ','.join(md.identification.keywords))
+    _set(context, serviceobj, 'pycsw:Creator', md.provider.contact.name)
+    _set(context, serviceobj, 'pycsw:Publisher', md.provider.name)
+    _set(context, serviceobj, 'pycsw:Contributor', md.provider.contact.name)
+    _set(context, serviceobj, 'pycsw:OrganizationName', md.provider.contact.name)
+    _set(context, serviceobj, 'pycsw:AccessConstraints', md.identification.accessconstraints)
+    _set(context, serviceobj, 'pycsw:OtherConstraints', md.identification.fees)
+    _set(context, serviceobj, 'pycsw:Source', record)
+    _set(context, serviceobj, 'pycsw:Format', md.identification.type)
+
+    for c in md.contents:
+
+        if md.contents[c].parent is None:
+            bbox = md.contents[c].boundingBoxWGS84
+            tmp = '%s,%s,%s,%s' % (bbox[0], bbox[1], bbox[2], bbox[3])
+            _set(context, serviceobj, 'pycsw:BoundingBox', util.bbox2wktpolygon(tmp))
+            break
+    _set(context, serviceobj, 'pycsw:CRS', 'urn:ogc:def:crs:EPSG:6.11:4326')
+    _set(context, serviceobj, 'pycsw:DistanceUOM', 'degrees')
+    _set(context, serviceobj, 'pycsw:ServiceType', 'OGC:WMTS')
+    _set(context, serviceobj, 'pycsw:ServiceTypeVersion', md.identification.version)
+    _set(context, serviceobj, 'pycsw:Operation', ','.join([d.name for d in md.operations]))
+    _set(context, serviceobj, 'pycsw:OperatesOn', ','.join(list(md.contents)))
+    _set(context, serviceobj, 'pycsw:CouplingType', 'tight')
+
+    links = [
+        '%s,OGC-WMTS Web Map Service,OGC:WMTS,%s' % (identifier, md.url),
+    ]
+
+    _set(context, serviceobj, 'pycsw:Links', '^'.join(links))
+    _set(context, serviceobj, 'pycsw:XML', caps2iso(serviceobj, md, context))
+
+    recobjs.append(serviceobj)
+
+    # generate record for each layer
+
+    LOGGER.debug('Harvesting %d WMTS layers' % len(md.contents))
+
+    for layer in md.contents:
+        recobj = repos.dataset()
+        identifier2 = '%s-%s' % (identifier, md.contents[layer].name)
+        _set(context, recobj, 'pycsw:Identifier', identifier2)
+        _set(context, recobj, 'pycsw:Typename', 'csw:Record')
+        _set(context, recobj, 'pycsw:Schema', 'http://www.opengis.net/wmts/1.0')
+        _set(context, recobj, 'pycsw:MdSource', record)
+        _set(context, recobj, 'pycsw:InsertDate', util.get_today_and_now())
+        _set(context, recobj, 'pycsw:Type', 'dataset')
+        _set(context, recobj, 'pycsw:ParentIdentifier', identifier)
+        if md.contents[layer].title:
+             _set(context, recobj, 'pycsw:Title', md.contents[layer].title)
+        else:
+            _set(context, recobj, 'pycsw:Title', "")
+        if md.contents[layer].abstract:
+            _set(context, recobj, 'pycsw:Abstract', md.contents[layer].abstract)
+        else:
+            _set(context, recobj, 'pycsw:Abstract', "")
+        if md.contents[layer].keywords:
+            _set(context, recobj, 'pycsw:Keywords', ','.join(md.contents[layer].keywords))
+        else:
+            _set(context, recobj, 'pycsw:Keywords', "")
+
+        _set(context, recobj, 'pycsw:AnyText',
+             util.get_anytext([md.contents[layer].title,
+                              md.contents[layer].abstract,
+                             ','.join(md.contents[layer].keywords)
+                             ]))
+
+        bbox = md.contents[layer].boundingBoxWGS84
+
+	if bbox is not None:
+            tmp = '%s,%s,%s,%s' % (bbox[0], bbox[1], bbox[2], bbox[3])
+            _set(context, recobj, 'pycsw:BoundingBox', util.bbox2wktpolygon(tmp))
+            _set(context, recobj, 'pycsw:CRS', 'urn:ogc:def:crs:EPSG:6.11:4326')
+            _set(context, recobj, 'pycsw:DistanceUOM', 'degrees')
+        else:
+            bbox = md.contents[layer].boundingBox
+            if bbox:
+                tmp = '%s,%s,%s,%s' % (bbox[0], bbox[1], bbox[2], bbox[3])
+                _set(context, recobj, 'pycsw:BoundingBox', util.bbox2wktpolygon(tmp))
+                _set(context, recobj, 'pycsw:CRS', 'urn:ogc:def:crs:EPSG:6.11:%s' % \
+                bbox[-1].split(':')[1])
+
+
+        params = {
+            'service': 'WMTS',
+            'version': '1.0.0',
+            'request': 'GetTile',
+            'layer': md.contents[layer].name,
+            'keywords': md.contents[layer].keywords
+        }
+
+        links = [
+           '%s,OGC-Web Map Tile Service,OGC:WMTS,%s' % (md.contents[layer].name, md.url),
+            '%s,Web image thumbnail (URL),WWW:LINK-1.0-http--image-thumbnail,%s' % (md.contents[layer].name, build_get_url(md.url, params))
+        ]
+
+        _set(context, recobj, 'pycsw:Links', '^'.join(links))
+        _set(context, recobj, 'pycsw:XML', caps2iso(recobj, md, context))
+
+        recobjs.append(recobj)
+
+    return recobjs
+
 
 def _parse_wfs(context, repos, record, identifier):
 
