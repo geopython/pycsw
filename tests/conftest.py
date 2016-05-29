4aca3bd38c84efc03051d3da2a8b44a694614569
Ricardo Silva <ricardo.garcia.silva@gmail.com>
2016-05-29 11:32:39 +0100

    adding ports for each suite

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 """pytest configuration file"""
 
+from configparser import ConfigParser
 import glob
 import os
 import subprocess
-import random
 import shlex
 import time
 
@@ -47,6 +47,18 @@ def pytest_addoption(parser):
         default="sqlite",
         help="Database backend to use when performing functional tests"
     )
+    parser.addoption(
+        "--database-user-postgres",
+        default="postgres",
+        help="Username to use for creating and accessing local postgres "
+             "databases used for functional tests."
+    )
+    parser.addoption(
+        "--database-password-postgres",
+        default="",
+        help="Password to use for creating and accessing local postgres "
+             "databases used for functional tests."
+    )
     for suite_name in FUNCTIONAL_SUITES:
         parser.addoption(
             "--server-url-{0}-suite".format(suite_name),
@@ -68,7 +80,7 @@ def pytest_generate_tests(metafunc):
 
     expected_dir = os.path.join(TESTS_ROOT, "expected")
     for suite_name in FUNCTIONAL_SUITES:
-        safe_suite_name = suite_name.replace("-", "_")
+        safe_suite_name = _get_suite_safe_name(suite_name)
         option_name = "--server-url-{0}-suite".format(suite_name)
         fixture_name = "server_{0}_suite".format(safe_suite_name)
         test_name = "test_{0}_suite".format(safe_suite_name)
@@ -108,78 +120,93 @@ def suite_info(request):
 
 
 @pytest.fixture(scope="session")
-def server_apiso_suite(request):
-    return _get_server_with_config(request, "apiso")
+def server_apiso_suite(request, tmpdir_factory):
+    return _get_server_with_config(request, "apiso",
+                                   tmpdir_factory, 8010)
 
 
 @pytest.fixture(scope="session")
-def server_apiso_inspire_suite(request):
-    return _get_server_with_config(request, "apiso-inspire")
+def server_apiso_inspire_suite(request, tmpdir_factory):
+    return _get_server_with_config(request, "apiso-inspire",
+                                   tmpdir_factory, 8011)
 
 
 @pytest.fixture(scope="session")
-def server_atom_suite(request):
-    return _get_server_with_config(request, "atom")
+def server_atom_suite(request, tmpdir_factory):
+    return _get_server_with_config(request, "atom",
+                                   tmpdir_factory, 8012)
 
 
 @pytest.fixture(scope="session")
-def server_cite_suite(request):
-    return _get_server_with_config(request, "cite")
+def server_cite_suite(request, tmpdir_factory):
+    return _get_server_with_config(request, "cite",
+                                   tmpdir_factory, 8013)
 
 
 @pytest.fixture(scope="session")
-def server_csw30_suite(request):
-    return _get_server_with_config(request, "csw30")
+def server_csw30_suite(request, tmpdir_factory):
+    return _get_server_with_config(request, "csw30",
+                                   tmpdir_factory, 8014)
 
 
 @pytest.fixture(scope="session")
-def server_default_suite(request):
-    return _get_server_with_config(request, "default")
+def server_default_suite(request, tmpdir_factory):
+    return _get_server_with_config(request, "default",
+                                   tmpdir_factory, 8015)
 
 
 @pytest.fixture(scope="session")
-def server_dif_suite(request):
-    return _get_server_with_config(request, "dif")
+def server_dif_suite(request, tmpdir_factory):
+    return _get_server_with_config(request, "dif",
+                                   tmpdir_factory, 8016)
 
 
 @pytest.fixture(scope="session")
-def server_ebrim_suite(request):
-    return _get_server_with_config(request, "ebrim")
+def server_ebrim_suite(request, tmpdir_factory):
+    return _get_server_with_config(request, "ebrim",
+                                   tmpdir_factory, 8017)
 
 
 @pytest.fixture(scope="session")
-def server_fgdc_suite(request):
-    return _get_server_with_config(request, "fgdc")
+def server_fgdc_suite(request, tmpdir_factory):
+    return _get_server_with_config(request, "fgdc",
+                                   tmpdir_factory, 8018)
 
 
 @pytest.fixture(scope="session")
-def server_gm03_suite(request):
-    return _get_server_with_config(request, "gm03")
+def server_gm03_suite(request, tmpdir_factory):
+    return _get_server_with_config(request, "gm03",
+                                   tmpdir_factory, 8019)
 
 
 @pytest.fixture(scope="session")
-def server_harvesting_suite(request):
-    return _get_server_with_config(request, "harvesting")
+def server_harvesting_suite(request, tmpdir_factory):
+    return _get_server_with_config(request, "harvesting",
+                                   tmpdir_factory, 8020)
 
 
 @pytest.fixture(scope="session")
-def server_oaipmh_suite(request):
-    return _get_server_with_config(request, "oaipmh")
+def server_oaipmh_suite(request, tmpdir_factory):
+    return _get_server_with_config(request, "oaipmh",
+                                   tmpdir_factory, 8021)
 
 
 @pytest.fixture(scope="session")
-def server_repofilter_suite(request):
-    return _get_server_with_config(request, "repofilter")
+def server_repofilter_suite(request, tmpdir_factory):
+    return _get_server_with_config(request, "repofilter",
+                                   tmpdir_factory, 8022)
 
 
 @pytest.fixture(scope="session")
-def server_sru_suite(request):
-    return _get_server_with_config(request, "sru")
+def server_sru_suite(request, tmpdir_factory):
+    return _get_server_with_config(request, "sru",
+                                   tmpdir_factory, 8023)
 
 
 @pytest.fixture(scope="session")
-def server_utf_8_suite(request):
-    return _get_server_with_config(request, "utf-8")
+def server_utf_8_suite(request, tmpdir_factory):
+    return _get_server_with_config(request, "utf-8",
+                                   tmpdir_factory, 8024)
 
 
 def _configure_functional_post_tests(expected_dir, suite_name):
@@ -199,29 +226,63 @@ def _configure_functional_post_tests(expected_dir, suite_name):
     return test_data, test_names
 
 
-def _get_server_with_config(request, suite_name):
+def _get_suite_safe_name(suite_name):
+    return suite_name.replace("-", "_")
+
+
+def _get_server_with_config(request, suite_name, tmpdir_factory, port_number):
     url = request.config.getoption("--server-url-{0}-suite".format(suite_name))
-    config = os.path.join(TESTS_ROOT, "suites", suite_name, "default.cfg")
     if url is None:
-        url = _start_local_server(request, config)
+        print("Initializing server for {0} suite...".format(suite_name))
+        original_config_path = os.path.join(TESTS_ROOT, "suites",
+                                            suite_name, "default.cfg")
+        safe_suite_name = _get_suite_safe_name(suite_name)
+        config = ConfigParser()
+        config.read(original_config_path)
+        test_temp_directory = tmpdir_factory.mktemp(safe_suite_name,
+                                                    numbered=True)
+        db_url = _get_database_url(request, safe_suite_name,
+                                   test_temp_directory)
+        # now we can change the config as needed
+        config.set("repository", "database", db_url)
+        new_config = test_temp_directory.join("default.cfg")
+        fh = new_config.open("w")
+        config.write(fh)
+        fh.close()
+        # create the database, if needed
+        # load records, if any
+        url = _start_local_server(request, str(new_config), port_number)
     else:  # use the provided url and assume the config has been set
         url = url
     return url
 
 
-def _start_local_server(request, config_path):
-    port = random.randint(8000, 8050)
-    command = "python pycsw/wsgi.py {}".format(port)
+def _get_database_url(request, suite_name, test_dir):
+    db_type = request.config.getoption("--database-backend")
+    if db_type == "sqlite":
+        db_url = "sqlite:///{0}/records.db".format(test_dir)
+    elif db_type == "postgres":
+        user = request.config.getoption("--database-user-postgres")
+        password = request.config.getoption("--database-password-postgres")
+        db_url = "postgres://{0}:{1}@localhost/pycsw_test_{2}".format(
+            user, password, suite_name)
+    else:
+        raise NotImplementedError
+    return db_url
+
+
+def _start_local_server(request, config_path, port_number):
+    command = "python pycsw/wsgi.py {}".format(port_number)
     working_dir = os.path.dirname(TESTS_ROOT)
     env = os.environ.copy()
     env["PYCSW_CONFIG"] = config_path
     pycsw_process = subprocess.Popen(shlex.split(command), cwd=working_dir,
                                      env=env)
-    time.sleep(2)  # give the external process some time to start
+    time.sleep(3)  # give the external process some time to start
 
     def finalizer():
         pycsw_process.terminate()
         pycsw_process.wait()
 
     request.addfinalizer(finalizer)
-    return "http://localhost:{}".format(port)
+    return "http://localhost:{}".format(port_number)
