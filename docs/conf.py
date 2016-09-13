7b08c0692d8003b9836c07a9aeb59664c20c05bc
Tom Kralidis <tomkralidis@gmail.com>
2016-09-13 19:28:51 -0400

    fix rtd build [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -322,3 +322,21 @@ epub_copyright = copyright
 
 # Allow duplicate toc entries.
 #epub_tocdup = True
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return Mock
+        else:
+            return Mock()
+
+MOCK_MODULES = ['shapely']
