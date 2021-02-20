24abfd557fb90bc4b435f54e2f36254e6db94bea
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2021-02-20 17:49:18 +0200

    Starting WSGI flask application

---
+++ pycsw/wsgi_flask.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# =================================================================
+#
+# Authors: Tom Kralidis <tomkralidis@gmail.com>
+#          Angelos Tzotsos <tzotsos@gmail.com>
+#
+# Copyright (c) 2021 Tom Kralidis
+# Copyright (c) 2021 Angelos Tzotsos
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# =================================================================
+
+import os
+from flask import Flask, Blueprint, make_response, request, send_from_directory
+from pycsw import server
+
+@BLUEPRINT.route('/')
+def landing_page():
+    """
+    OGC API landing page endpoint
+
+    :returns: HTTP response
+    """
+    headers, status_code, content = api_.landing_page(
+        request.headers, request.args)
+
+    response = make_response(content, status_code)
+
+    if headers:
+        response.headers = headers
+
+    return response
+
+@BLUEPRINT.route('/csw')
+def landing_page():
+    """
+    CSW endpoint
+
+    :returns: HTTP response
+    """
+    headers, status_code, content = api_.landing_page(
+        request.headers, request.args)
+
+    response = make_response(content, status_code)
+
+    if headers:
+        response.headers = headers
+
+    return response
