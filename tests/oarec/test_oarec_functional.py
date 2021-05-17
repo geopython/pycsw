38649102c315db2e3420425721a2f5252d1f57ca
Ricardo Garcia Silva <ricardo@kartoza.com>
2021-05-17 08:38:52 +0100

    Refactor initial tests to a single test

--- tests/oarec/test_oarec_functional.py
+++ tests/oarec/test_oarec_functional.py
@@ -8,13 +8,29 @@ pytestmark = [
 ]
 
 
-@pytest.mark.parametrize("f_query_param, accept_header", [
-    pytest.param("json", "text/html"),
-    pytest.param(None, "text/html"),
-    pytest.param("json", "application/json"),
-    pytest.param(None, "application/json"),
+@pytest.mark.parametrize("endpoint, f_query_param, accept_header", [
+    pytest.param("/", "json", "text/html"),
+    pytest.param("/", None, "text/html"),
+    pytest.param("/", "json", "application/json"),
+    pytest.param("/", None, "application/json"),
+    pytest.param("/openapi", "json", "text/html"),
+    pytest.param("/openapi", None, "text/html"),
+    pytest.param("/openapi", "json", "application/json"),
+    pytest.param("/openapi", None, "application/json"),
+    pytest.param("/conformance", "json", "text/html"),
+    pytest.param("/conformance", None, "text/html"),
+    pytest.param("/conformance", "json", "application/json"),
+    pytest.param("/conformance", None, "application/json"),
+    pytest.param("/collections", "json", "text/html"),
+    pytest.param("/collections", None, "text/html"),
+    pytest.param("/collections", "json", "application/json"),
+    pytest.param("/collections", None, "application/json"),
+    pytest.param("/collections/metadata:main", "json", "text/html"),
+    pytest.param("/collections/metadata:main", None, "text/html"),
+    pytest.param("/collections/metadata:main", "json", "application/json"),
+    pytest.param("/collections/metadata:main", None, "application/json"),
 ])
-def test_landing_page_renders(oarec_client, f_query_param, accept_header):
+def test_page_renders(oarec_client, endpoint, f_query_param, accept_header):
     query = {}
     if f_query_param is not None:
         query["f"] = f_query_param
@@ -22,80 +38,5 @@ def test_landing_page_renders(oarec_client, f_query_param, accept_header):
     if accept_header is not None:
         headers["Accept"] = accept_header
     response = oarec_client.get(
-        "/", query_string=query or None, headers=headers or None)
-    assert response.status_code == HTTPStatus.OK
-
-
-@pytest.mark.parametrize("f_query_param, accept_header", [
-    pytest.param("json", "text/html"),
-    pytest.param(None, "text/html"),
-    pytest.param("json", "application/json"),
-    pytest.param(None, "application/json"),
-])
-def test_openapi_page_renders(oarec_client, f_query_param, accept_header):
-    query = {}
-    if f_query_param is not None:
-        query["f"] = f_query_param
-    headers = {}
-    if accept_header is not None:
-        headers["Accept"] = accept_header
-    response = oarec_client.get(
-        "/openapi", query_string=query or None, headers=headers or None)
-    assert response.status_code == HTTPStatus.OK
-
-
-@pytest.mark.parametrize("f_query_param, accept_header", [
-    pytest.param("json", "text/html"),
-    pytest.param(None, "text/html"),
-    pytest.param("json", "application/json"),
-    pytest.param(None, "application/json"),
-])
-def test_conformance_page_renders(oarec_client, f_query_param, accept_header):
-    query = {}
-    if f_query_param is not None:
-        query["f"] = f_query_param
-    headers = {}
-    if accept_header is not None:
-        headers["Accept"] = accept_header
-    response = oarec_client.get(
-        "/conformance", query_string=query or None, headers=headers or None)
-    assert response.status_code == HTTPStatus.OK
-
-
-@pytest.mark.parametrize("f_query_param, accept_header", [
-    pytest.param("json", "text/html"),
-    pytest.param(None, "text/html"),
-    pytest.param("json", "application/json"),
-    pytest.param(None, "application/json"),
-])
-def test_collections_page_renders(oarec_client, f_query_param, accept_header):
-    query = {}
-    if f_query_param is not None:
-        query["f"] = f_query_param
-    headers = {}
-    if accept_header is not None:
-        headers["Accept"] = accept_header
-    response = oarec_client.get(
-        "/collections", query_string=query or None, headers=headers or None)
-    assert response.status_code == HTTPStatus.OK
-
-
-@pytest.mark.parametrize("f_query_param, accept_header", [
-    pytest.param("json", "text/html"),
-    pytest.param(None, "text/html"),
-    pytest.param("json", "application/json"),
-    pytest.param(None, "application/json"),
-])
-def test_collection_page_renders(oarec_client, f_query_param, accept_header):
-    query = {}
-    if f_query_param is not None:
-        query["f"] = f_query_param
-    headers = {}
-    if accept_header is not None:
-        headers["Accept"] = accept_header
-    response = oarec_client.get(
-        "/collections/metadata:main",
-        query_string=query or None,
-        headers=headers or None
-    )
+        endpoint, query_string=query or None, headers=headers or None)
     assert response.status_code == HTTPStatus.OK
