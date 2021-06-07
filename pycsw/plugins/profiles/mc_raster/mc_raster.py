8827a98f9ca346e920442bdde32b4831524d002b
Shlomi k <65117898+CL-SHLOMIKONCHA@users.noreply.github.com>
2021-06-07 17:19:45 +0300

    chore: added raster profile (#8)
    * chore: added raster profile
    
    * fix: fixed id queryable param
    
    * Update pycsw-deployment.yaml
    
    * fix(profiles): profile fields synching
    
    * fix(mappings): must fields added
    
    * feat: reveted helm chage
    
    * chore: updated xsd elements
    
    * chore: added xsd elements
    
    * fix: removed duplicate element
    
    Co-authored-by: yossiz16 <yossiz16@gmail.com>
    Co-authored-by: alebinson <newdarkvirus@gmail.com>
    Co-authored-by: Shahar s <cl_shaharshavit@rnd-hub.com>

---
+++ pycsw/plugins/profiles/mc_raster/mc_raster.py
@@ -0,0 +1,67 @@
+from pycsw.plugins.profiles.base_profile.models.models import ProfileRepository, Queryable, TypenameModel
+from pycsw.plugins.profiles.base_profile import base_profile
+
+
+class MCRaster(base_profile.base_profile):
+    ''' MCRaster class '''
+    
+    def __init__(self, model, namespaces, context):
+
+        prefix = 'mc'
+        typename = '%s:MCRasterRecord' % (prefix)
+        main_namespace = 'http://schema.mapcolonies.com/raster'
+
+        schemas_pathes = [[ 'plugins','profiles', 'mc_raster', 'schemas', 'mcraster-record.xsd']]
+
+        added_namespaces = {
+            prefix: main_namespace
+        }
+
+        profileRepository: ProfileRepository = {
+            typename: TypenameModel(
+                added_namespaces[prefix],
+                {
+                    'MCRasterQueryables': {
+                        'mc:id': Queryable(context.md_core_model['mappings']['pycsw:Identifier'], 'mc:id'),
+                        'mc:productId': Queryable(context.md_core_model['mappings']['pycsw:ProductId'], 'mc:productId'),
+                        'mc:productName': Queryable(context.md_core_model['mappings']['pycsw:Title'], 'mc:productName'),
+                        'mc:productVersion': Queryable(context.md_core_model['mappings']['pycsw:ProductVersion'], 'mc:productVersion'),
+                        'mc:productType': Queryable(context.md_core_model['mappings']['pycsw:ProductType'], 'mc:productType'),
+                        'mc:description': Queryable(context.md_core_model['mappings']['pycsw:Abstract'], 'mc:description'),
+                        'mc:creationDate': Queryable(context.md_core_model['mappings']['pycsw:CreationDate'], 'mc:creationDate'),
+                        'mc:ingestionDate': Queryable(context.md_core_model['mappings']['pycsw:IngestionDate'], 'mc:ingestionDate'),
+                        'mc:updateDate': Queryable(context.md_core_model['mappings']['pycsw:UpdateDate'], 'mc:updateDate'),
+                        'mc:imagingTime_begin': Queryable(context.md_core_model['mappings']['pycsw:TempExtent_begin'], 'mc:imagingTime_begin'),
+                        'mc:imagingTime_end': Queryable(context.md_core_model['mappings']['pycsw:TempExtent_end'], 'mc:imagingTime_end'),
+                        'mc:resolution': Queryable(context.md_core_model['mappings']['pycsw:Resolution'], 'mc:resolution'),
+                        'mc:horizontalAccuracyCE90': Queryable(context.md_core_model['mappings']['pycsw:horizontalAccuracyCE90'], 'mc:horizontalAccuracyCE90'),
+                        'mc:sensorType': Queryable(context.md_core_model['mappings']['pycsw:sensorType'], 'mc:sensorType'),
+                        'mc:SRS': Queryable(context.md_core_model['mappings']['pycsw:CRS'], 'mc:SRS'),
+                        'mc:SRSName': Queryable(context.md_core_model['mappings']['pycsw:CRSName'], 'mc:SRSName'),
+                        'mc:region': Queryable(context.md_core_model['mappings']['pycsw:Region'], 'mc:region'),
+                        'mc:classification': Queryable(context.md_core_model['mappings']['pycsw:Classification'], 'mc:classification'),
+                        'mc:links': Queryable(context.md_core_model['mappings']['pycsw:Links'], 'mc:links'),
+                        'mc:type': Queryable(context.md_core_model['mappings']['pycsw:Type'], 'mc:type'),
+                        'mc:boundingBox': Queryable(context.md_core_model['mappings']['pycsw:BoundingBox'], 'mc:boundingBox'),
+                        'mc:footprint': Queryable(context.md_core_model['mappings']['pycsw:footprint'], 'mc:footprint'),
+                        'mc:keywords': Queryable(context.md_core_model['mappings']['pycsw:Keywords'], 'mc:keywords'),
+                        'mc:rms': Queryable(context.md_core_model['mappings']['pycsw:Rms'], 'mc:rms'),
+                        'mc:scale': Queryable(context.md_core_model['mappings']['pycsw:Scale'], 'mc:scale'),
+                    }
+                })
+        }
+
+        super().__init__(name='mcraster',
+                         version='1.0.0',
+                         title='Map Colonies raster profile of CSW',
+                         url='https://github.com/MapColonies',
+                         typename=typename,
+                         model=model,
+                         core_namespaces=namespaces,
+                         added_namespaces=added_namespaces,
+                         repositories=profileRepository,
+                         schemas_paths=schemas_pathes,
+                         context=context,
+                         prefix=prefix,
+                         main_namespace=main_namespace
+                         )
