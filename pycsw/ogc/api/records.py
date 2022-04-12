9f1ef474e666528414e8592831553ea0de2a8563
Ricardo Garcia Silva <ricardo@kartoza.com>
2022-04-12 14:57:54 +0100

    Fix brittle retrieval of custom mappings

--- pycsw/ogc/api/records.py
+++ pycsw/ogc/api/records.py
@@ -110,7 +110,7 @@ class API:
         if self.config.has_option('repository', 'filter'):
             repo_filter = self.config.get('repository', 'filter')
 
-        custom_mappings_path = self.config.get('repository', 'mappings', None)
+        custom_mappings_path = self.config.get('repository', 'mappings', fallback=None)
         if custom_mappings_path is not None:
             md_core_model = load_custom_repo_mappings(custom_mappings_path)
             if md_core_model is not None:
