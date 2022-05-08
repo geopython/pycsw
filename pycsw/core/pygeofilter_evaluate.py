5ece1883948f49cfbc4b201c5ad0270cd29ee264
Tom Kralidis <tomkralidis@gmail.com>
2022-05-08 19:59:43 -0400

    move ILIKE handling to pygeofilter

--- pycsw/core/pygeofilter_evaluate.py
+++ pycsw/core/pygeofilter_evaluate.py
@@ -55,6 +55,20 @@ class PycswFilterEvaluator(SQLAlchemyFilterEvaluator):
         else:
             return text(f"query_spatial({geometry}, '{wkt}', 'bbox', 'false') = 'true'")  # noqa
 
+    @handle(ast.Like)
+    def ilike(self, node, lhs):
+        ci = not node.nocase
+
+        if self._pycsw_dbtype.startswith('sqlite'):
+            ci = node.nocase
+
+        return filters.like(
+            lhs,
+            node.pattern,
+            ci,
+            node.not_,
+        )
+
 
 def to_filter(ast, dbtype, field_mapping=None):
     return PycswFilterEvaluator(field_mapping, dbtype).evaluate(ast)
