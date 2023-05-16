16a29effe848068a79c3814612d69223612ea1af
Shlomi k <65117898+CL-SHLOMIKONCHA@users.noreply.github.com>
2023-05-16 11:44:55 +0300

    added token user details from payload (MAPCO-3062) (#46)
    * feat: added token user details to logs
    
    * feat: fix release name
    
    * feat: fix release name
    
    * fix: removed unnecessary func
    
    * fix: token validation

---
+++ helm/raster/config/jwt.js
@@ -0,0 +1,35 @@
+function jwt(data) {
+    if(data) {
+        var parts = data.split('.').slice(0,2)
+            .map(v=>Buffer.from(v, 'base64url').toString())
+            .map(JSON.parse);
+        return { headers:parts[0], payload: parts[1] };
+    } else {
+        return;
+    }
+}
+
+function jwt_payload_sub(r) {
+    try {
+        let token;
+        if(r.args["token"]) {
+          token = jwt(r.args["token"]);
+          if(!token) {
+            return "";
+          }
+          return token.payload.sub;
+        };
+        if(r.headersIn["x-api-key"]) {
+          token = jwt(r.headersIn["x-api-key"]);
+          if(!token) {
+            return "";
+          }
+          return token.payload.sub;
+        }
+        return "";
+    } catch (error) {
+        return "";
+    }
+}
+
+export default {jwt_payload_sub}
