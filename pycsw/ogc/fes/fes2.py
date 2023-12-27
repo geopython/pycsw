7e568cf546699b96fa00085d66ff7972d9098ced
Barel Shmuely <barel.shmuely@optibus.com>
2023-12-27 14:31:31 +0200

    fix parse of nested multiple binary ops (#782)

--- pycsw/ogc/fes/fes2.py
+++ pycsw/ogc/fes/fes2.py
@@ -254,7 +254,6 @@ def parse(element, queryables, dbtype, nsmap, orm='sqlalchemy', language='englis
         return expression
 
     queries = []
-    queries_nested = []
     values = []
 
     LOGGER.debug('Scanning children elements')
@@ -319,8 +318,10 @@ def parse(element, queryables, dbtype, nsmap, orm='sqlalchemy', language='englis
             tagname = ' %s ' % child_tag_name.lower()
             if tagname in [' or ', ' and ']:  # this is a nested binary logic query
                 LOGGER.debug('Nested binary logic detected; operator=%s', tagname)
+                queries_nested = []
                 for child2 in child.xpath('child::*'):
                     queries_nested.append(_get_comparison_expression(child2))
+                LOGGER.debug('Nested binary logic queries: %s', queries_nested)
                 queries.append('(%s)' % tagname.join(queries_nested))
             else:
                 queries.append(_get_comparison_expression(child))
