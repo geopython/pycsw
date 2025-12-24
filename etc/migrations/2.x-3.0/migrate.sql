2fdea23c234d55c45a6c4fcfe15316a57569c9c4
Tom Kralidis <tomkralidis@gmail.com>
2025-12-24 08:11:08 -0500

    add migrations and test DB updates

--- etc/migrations/2.x-3.0/migrate.sql
+++ etc/migrations/2.x-3.0/migrate.sql
@@ -4,4 +4,6 @@ alter table records add column edition TEXT;
 alter table records add column contacts TEXT;
 alter table records add column themes TEXT;
 alter table records add column illuminationelevationangle TEXT;
+alter table records alter column cloudcover type real using cast(cloudcover as float);
+alter table records alter column distancevalue type real using cast(distancevalue as float);
 vacuum;
