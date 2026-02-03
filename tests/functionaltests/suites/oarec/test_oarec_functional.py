94e701054cb22c7265b16146d76c52a30950b331
Tom Kralidis <tomkralidis@gmail.com>
2026-02-02 19:38:44 -0500

    update tests

--- tests/functionaltests/suites/oarec/test_oarec_functional.py
+++ tests/functionaltests/suites/oarec/test_oarec_functional.py
@@ -108,7 +108,7 @@ def test_queryables(config):
     assert content['$id'] == 'http://localhost/pycsw/oarec/collections/metadata:main/queryables'  # noqa
     assert content['$schema'] == 'http://json-schema.org/draft/2019-09/schema'
 
-    assert len(content['properties']) == 17
+    assert len(content['properties']) == 18
 
     assert 'geometry' in content['properties']
     assert content['properties']['geometry']['$ref'] == 'https://geojson.org/schema/Polygon.json'  # noqa
--- tests/functionaltests/suites/oarec/test_oarec_virtual_collections_functional.py
+++ tests/functionaltests/suites/oarec/test_oarec_virtual_collections_functional.py
@@ -116,7 +116,7 @@ def test_queryables(config_virtual_collections):
     assert content['$id'] == 'http://localhost/pycsw/oarec/collections/metadata:main/queryables'  # noqa
     assert content['$schema'] == 'http://json-schema.org/draft/2019-09/schema'
 
-    assert len(content['properties']) == 17
+    assert len(content['properties']) == 18
 
     assert 'geometry' in content['properties']
     assert content['properties']['geometry']['$ref'] == 'https://geojson.org/schema/Polygon.json'  # noqa
